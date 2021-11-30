37c6ff27fcb95eb5d18efe003f87bc4756ec4da6
Dirk Feldmann <dirkf94@gmail.com>
2021-11-30 17:16:57 +0100

    c and requirements  chnaged

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+from app import schemas
 from .routers import post, user, auth, vote
 
 # models.Base.metadata.create_all(bind=engine)  überflüssig wenn man alembic benutzt
@@ -22,10 +24,6 @@ app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
 
-@app.get("/")
+@app.get("/", response_model=schemas.Message)
 def root():
     return {"message": "This is only the root."}
-
-
-
-
