1aa14f71f21ee786612cb2887c656180058746d6
Dirk Feldmann <dirkf94@gmail.com>
2021-12-02 13:49:07 +0100

    works with pgAdmin

--- app/models.py
+++ app/models.py
@@ -25,5 +25,5 @@ class User(Base):
 class Vote(Base):
     __tablename__ = "votes"
 
-    post_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
-    user_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
